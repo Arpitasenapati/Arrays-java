5ee0843d929b7a95da01046f970224b318890460
Arpitasenapati <108747344+Arpitasenapati@users.noreply.github.com>
2023-08-05 15:19:50 +0530

    Add files via upload

---
+++ SubArrays.java
@@ -0,0 +1,22 @@
+
+public class SubArrays {
+    static void subarray(int arr[]) {
+    	for(int i=0;i<arr.length;i++) {
+    		for(int j=i;j<arr.length;j++) {
+    			for(int k=i;k<=j;k++) {
+    			  System.out.print(arr[k]+" ");
+    			}
+    			System.out.println();
+    		}
+    		System.out.println();
+    	}
+    	
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {2,4,6,8,10};
+        subarray(arr);
+	}
+
+}
